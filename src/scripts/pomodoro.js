410d5e34a46eeeff564f57e5064ce28648687014
Almir <almirsamad@gmail.com>
2026-09-22 16:11:49 -0400

    Aula 04

---
+++ src/scripts/pomodoro.js
@@ -0,0 +1,31 @@
+const tabs = document.querySelectorAll('.timer-card__tab');
+
+const page = document.querySelector('.page');
+
+const modeImage = document.querySelector('.banner img');
+
+let mode = 1;
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', (event) => {
+    console.log('elemento clicado', event.currentTarget);
+
+    mode = event.currentTarget.dataset.mode;
+    page.dataset.mode = mode;
+    console.log('novo modo', mode);
+
+    tabs.forEach((tab) => {
+      tab.disabled = false;
+      tab.classList.remove('timer-card__tab--active');
+      tab.setAttribute('aria-selected', false);
+    });
+
+    const imageModeSrc = `/mode-${mode}.png`;
+
+    modeImage.setAttribute('src', imageModeSrc);
+
+    event.currentTarget.disabled = true;
+    event.currentTarget.classList.add('timer-card__tab--active');
+    event.currentTarget.setAttribute('aria-selected', true);
+  });
+});
---
+++ src/scripts/sounds.js
@@ -0,0 +1,13 @@
+const toggle = document.querySelector('#toggle-music');
+
+const audio = new Audio('/sounds/lofi.mp3');
+audio.loop = true;
+
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+});
+console.log('manipular os sons!!', toggle);
