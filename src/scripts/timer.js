46eb8dc6d02d0aefb499de6c80a2693d47438eb5
Almir <almirsamad@gmail.com>
2026-09-23 16:32:55 -0400

    Aula 06

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -1,6 +1,6 @@
-const FOCUS_TIME = 25;
+const FOCUS_TIME = 25 * 60;
 const SHORT_BREAK_TIME = 5;
-const LONG_BREAK_TIME = 15;
+const LONG_BREAK_TIME = 15 * 60;
 
 const context = {
   1: FOCUS_TIME,
@@ -10,10 +10,50 @@ const context = {
 
 const timerDisplay = document.querySelector('.timer-card__time');
 const timerForm = document.querySelector('.timer-card__form');
-const timerButtonIcon = document.querySelector('timer-card__form .icon');
+const timerButtonIcon = document.querySelector('.timer-card__form .icon');
+const timerButton = timerForm.querySelector('.button');
+
+let currentMode = 1;
+let remainingSeconds = FOCUS_TIME;
+let intervalId;
+const audio = new Audio('sounds/ta-da.mp3');
+
+function startTimer() {
+  intervalId = setInterval(() => {
+    remainingSeconds--;
+    timerDisplay.textContent = formatTimer(remainingSeconds);
+
+    if (remainingSeconds < 0) {
+      pauseTimer();
+      resetTimer(currentMode);
+      audio.play();
+    }
+  }, 1000);
+
+  timerButtonIcon.classList.replace('icon--play', 'icon--pause');
+  timerButton.lastChild.textContent = 'Pausar';
+}
+
+function pauseTimer() {
+  clearInterval(intervalId);
+  intervalId = null;
+  timerButtonIcon.classList.replace('icon--pause', 'icon--play');
+  timerButton.lastChild.textContent = 'Continuar';
+}
+
+timerForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const isRunning = timerButtonIcon.classList.contains('icon--pause');
+
+  if (intervalId) {
+    pauseTimer();
+  } else {
+    startTimer();
+  }
+});
 
 function formatTimer(seconds) {
-  const data = new Date(seconds * 60 * 1000);
+  const data = new Date(seconds * 1000);
 
   return data.toLocaleTimeString('pt-Br', {
     minute: '2-digit',
@@ -23,6 +63,10 @@ function formatTimer(seconds) {
 }
 
 export function resetTimer(mode) {
-  const seconds = context[mode];
-  timerDisplay.textContent = formatTimer(seconds);
+  currentMode = mode;
+  if (intervalId) {
+    pauseTimer();
+  }
+  remainingSeconds = context[mode];
+  timerDisplay.textContent = formatTimer(remainingSeconds);
 }
